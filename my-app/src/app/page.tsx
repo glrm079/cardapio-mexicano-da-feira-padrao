a31954a4d737fc7799e379d4abe2afb42745b47d
Guilherme Oliveira <glrm07928@gmail.com>
2025-01-07 17:34:03 -0300

    NEW
    adding code

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,23 @@
+
+
+export default function Home() {
+  return (
+    <div className="mainDiv flex items-center  flex-col justify-between h-full">
+      {/* logo */}
+      <img className="w-36 p-5" src="/logo.png" alt="" />
+
+      {/* imagem em destaque  */}
+      <img className="mainImage" src="/main-image.png" alt="" />
+
+      {/* rodapé da pagina */}
+      <div className="flex flex-col justify-center items-center pb-4 z-40">
+        {/* botão ver mais */}
+        <a href="">
+          <img className="h-14 " src="/down-arrow-group.png" alt="" />
+        </a>
+      </div>
+    <img className="absolute bottom-0 w-full h-24 z-10 bottom-colors-desktop" src="/bottom-colors.png" alt="" />
+    <img className="absolute bottom-0 w-full h-44 z-10 bottom-colors-mobal" src="/bottom-colors-mobal.png" alt="" />
+    </div>
+  );
+}
