1ce884bfb3ea29c311ae9f207404eab751d9718b
Guilherme Oliveira <glrm07928@gmail.com>
2025-02-04 18:15:08 -0300

    .

--- my-app/src/app/cardapio/page.tsx
+++ my-app/src/app/cardapio/page.tsx
@@ -1,4 +1,4 @@
-
+import items from "/"; // Certifique-se de que o caminho do JSON está correto
 
 export default function Home() {
   return (
@@ -6,12 +6,28 @@ export default function Home() {
       <div className="flex w-full justify-center">
         <img src="/cardapio.png" alt="Cardápio" />
       </div>
-      <div className="itemImpar h- w-full relative">
-        <img className="absolute bottom-0 w-full h-2/4 z-10 " src="/Fundo verde.png" alt="" />
-      </div>
-      <div className="itemImpar h- w-full relative">
-        <img className="absolute top-0 w-full h-2/4 z-10 " src="/Fundo-verde-imp.png" alt="" />
-      </div>
+
+      {/* Renderização da lista de itens */}
+      {items.map((element) => {
+        // Verifica se o índice é par ou ímpar
+        return element.indice % 2 === 0 ? (
+          <div key={element.indice} className="itemImpar h- w-full relative">
+            <img
+              className="absolute bottom-0 w-full h-2/4 z-10"
+              src="/Fundo verde.png"
+              alt="Fundo verde"
+            />
+          </div>
+        ) : (
+          <div key={element.indice} className="itemImpar h- w-full relative">
+            <img
+              className="absolute top-0 w-full h-2/4 z-10"
+              src="/Fundo-verde-imp.png"
+              alt="Fundo verde invertido"
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
