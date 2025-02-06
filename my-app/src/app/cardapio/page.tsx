fe7ca2d313fd192b1293487282f231c2c3d87e0e
Guilherme Oliveira <glrm07928@gmail.com>
2025-02-06 17:55:00 -0300

    .

--- my-app/src/app/cardapio/page.tsx
+++ my-app/src/app/cardapio/page.tsx
@@ -1,33 +1,34 @@
-import items from "/"; // Certifique-se de que o caminho do JSON está correto
+
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <div className="flex w-full justify-center">
         <img src="/cardapio.png" alt="Cardápio" />
-      </div>
-
-      {/* Renderização da lista de itens */}
-      {items.map((element) => {
-        // Verifica se o índice é par ou ímpar
-        return element.indice % 2 === 0 ? (
-          <div key={element.indice} className="itemImpar h- w-full relative">
+      </div><
+        div  className="itemImpar h- w-full relative">
+            <div className="flex justify-center items-center z-20 flex-col">
+              <img className="z-20" src="/img-combo.png" alt="" />
+              <img  className="z-20 w-52" src="/combos-titulo.png" alt="" />
+            </div>
             <img
-              className="absolute bottom-0 w-full h-2/4 z-10"
+              className="absolute bottom-0 w-full h-2/4"
               src="/Fundo verde.png"
               alt="Fundo verde"
             />
           </div>
-        ) : (
-          <div key={element.indice} className="itemImpar h- w-full relative">
+          <div className="itemImpar h- w-full relative">
+            <div className="flex justify-center items-center z-20 flex-col">
+              <img className="z-20" src="/img-combo.png" alt="" />
+              <img  className="z-20 w-52" src="/combos-titulo.png" alt="" />
+            </div>
             <img
-              className="absolute top-0 w-full h-2/4 z-10"
+              className="absolute top-0 w-full h-2/4"
               src="/Fundo-verde-imp.png"
               alt="Fundo verde invertido"
             />
           </div>
-        );
-      })}
+      
     </div>
   );
 }
